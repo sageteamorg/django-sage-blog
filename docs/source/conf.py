fb7dbda30cbe4f3a1cf6ebe0c4e984d912891e47
Sepehr Akbarzadeh <sa.goldeneagle@gmail.com>
2024-07-31 20:04:49 +0330

    build(version): update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath("../../"))
 project = "django-sage-blog"
 copyright = "2024, sepehr akbarzadeh"
 author = "sepehr akbarzadeh"
-release = "0.1.3"
+release = "0.1.4"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
